1453ddf2a55e337b3f8463a4c30e06e4d10809e0
GeorgeMacLennan <georg,ma6775@vusd.us>
2020-09-05 23:09:02 -0700

    Initial commit

---
+++ app/src/main/java/com/georgetec/counter/Counter.java
@@ -0,0 +1,29 @@
+package com.georgetec.counter;
+
+public class Counter {
+    int value;
+
+    public Counter (int value) {
+        this.value = value;
+    }
+
+    public Counter() {
+
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public void reset() {
+        value = 0;
+    }
+
+    public void increment() {
+        value++;
+    }
+}
---
+++ app/src/main/java/com/georgetec/counter/MainActivity.java
@@ -0,0 +1,33 @@
+package com.georgetec.counter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    Counter counter;
+    TextView counterTV;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        counter = new Counter();
+        counterTV = (TextView) findViewById(R.id.value_TextView);
+
+}
+
+        public void incrementCounter(View view){
+            counter.increment();
+            String newValue = String.valueOf(counter.getValue());
+            counterTV.setText(newValue);
+        }
+
+        public void resetCounter (View view) {
+            counterTV.setText("0");
+        }
+}
